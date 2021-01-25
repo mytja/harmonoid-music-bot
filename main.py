868e765e4b82b45f476067536fc185ce7b906611
mytja <52399966+mytja@users.noreply.github.com>
2021-01-25 11:25:45 +0100

    Added server fix

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ bot = commands.AutoShardedBot(command_prefix='!')
 
 harmonoid = hs.HarmonoidService()
 
-vc = None
+vc = []
+vc_id = []
 
 @bot.event
 async def on_ready():
@@ -28,6 +29,7 @@ async def download(ctx, *, arg):
 @bot.command()
 async def play(ctx, *, arg):
     global vc
+    global vc_id
     if ctx.author == bot.user:
         return
 
@@ -36,10 +38,21 @@ async def play(ctx, *, arg):
     channel = discord.utils.get(ctx.guild.channels, name="Music")
     channel_id = channel.id
     channel = bot.get_channel(channel_id)
-
-    if vc == None:
-        vc = await channel.connect()
-    vc.play(discord.FFmpegPCMAudio(filename), after=lambda e: print('[ffmpeg-player] Successfully summoned FFMPEG player!', e))
+    
+    server_id = ctx.message.guild.id
+    
+    if server_id not in vc_id:
+        print("[server-append] Server was not appended!")
+        vc1 = await channel.connect()
+        vc.append(vc1)
+        vc_id.append(server_id)
+    
+    vcid = vc_id.index(server_id)
+    
+    try:
+        vc[vcid].play(discord.FFmpegPCMAudio(filename), after=lambda e: print('[ffmpeg-player] Successfully summoned FFMPEG player!', e))
+    except Exception as e:
+        print(f"Failed to summon FFMPEG player - Exception: ", e)
     
     if (len(channel.members) == 1):
         await ctx.send("Come and join me in the voice channel")
@@ -48,25 +61,28 @@ async def play(ctx, *, arg):
 
 @bot.command()
 async def stop(ctx):
-    if vc != None:
-        if vc.is_playing():
-            vc.stop()
+    server_id = ctx.message.guild.id
+    if vc.index(server_id) != None:
+        if vc.index(server_id).is_playing():
+            vc.index(server_id).stop()
             await ctx.send("Okay")
         else:
             await ctx.send("Cannot stop! No song is playing!")
 
 @bot.command()
 async def pause(ctx):
-    if vc != None:
-        if vc.is_playing():
-            vc.pause()
+    server_id = ctx.message.guild.id
+    if vc.index(server_id) != None:
+        if vc.index(server_id).is_playing():
+            vc.index(server_id).pause()
             await ctx.send("Okay")
         else:
             await ctx.send("Cannot pause! No song is playing!")
 
 @bot.command()
 async def resume(ctx):
-    if vc != None:
+    server_id = ctx.message.guild.id
+    if vc.index(server_id) != None:
         vc.resume()
         await ctx.send("Okay")
 
