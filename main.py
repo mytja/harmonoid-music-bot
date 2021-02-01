963f337365213df7ec0fd0f2a4440175967e310c
Denis <raitonoberu@mail.ru>
2021-02-01 20:53:48 +0700

    Fix auto-disconnect and lyrics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-# bot.py
 import asyncio
+import aiofiles
 import os
 
 from discord.ext import commands
@@ -16,6 +16,7 @@ bot = commands.AutoShardedBot(command_prefix="!")
 harmonoid = hs.HarmonoidService()
 
 vc = []
+tc_id = []
 vc_id = []
 
 error = 0
@@ -24,10 +25,7 @@ error = 0
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} has connected to Discord!")
-    # try:
-    #     bot.loop.create_task(disconnectOnEmptyChannel())
-    # except:
-    #     logging.exception("\n\n--------\n\Disconnect on empty channel error: ")
+    asyncio.ensure_future(disconnectOnEmptyChannel(), loop=bot.loop)
 
 
 # @bot.command()
@@ -71,15 +69,16 @@ async def play(ctx, *, arg):
         await ctx.send("No voice channel called Music. Please create one.")
         return
 
-    server_id = ctx.message.guild.id
+    chat_id = ctx.message.guild.id
 
-    if server_id not in vc_id:
+    if channel_id not in vc_id:
         print("[server-append] Server was not appended!")
         vc1 = await channel.connect()
         vc.append(vc1)
-        vc_id.append(server_id)
+        tc_id.append(chat_id)
+        vc_id.append(channel_id)
 
-    vcid = vc_id.index(server_id)
+    vcid = vc_id.index(channel_id)
 
     try:
         vc[vcid].play(
@@ -131,7 +130,6 @@ async def playYT(ctx, *, arg):
         await ctx.send(
             f"Failed to get JavaScript from a player :sad: . Trying to continue. Code to report to maintainers: {error-1}"
         )
-
     if ctx.author == bot.user:
         return
 
@@ -154,21 +152,16 @@ async def playYT(ctx, *, arg):
         await ctx.send("No voice channel called Music... Please create one.")
         return
 
-    server_id = ctx.message.guild.id
+    chat_id = ctx.message.guild.id
 
-    if server_id not in vc_id:
-        try:
-            print("[server-append] Server was not appended!")
-            vc1 = await channel.connect()
-            vc.append(vc1)
-            vc_id.append(server_id)
-        except:
-            await ctx.send(
-                "Failed to join a voice channel :cry: . Our developers would have to reboot the server now... Server Administrators, please try to disconnect bot from a voice channel, and then use command !refresh"
-            )
-            return
+    if channel_id not in vc_id:
+        print("[server-append] Server was not appended!")
+        vc1 = await channel.connect()
+        vc.append(vc1)
+        tc_id.append(chat_id)
+        vc_id.append(channel_id)
 
-    vcid = vc_id.index(server_id)
+    vcid = vc_id.index(channel_id)
 
     try:
         vc[vcid].play(
@@ -209,10 +202,10 @@ async def playYT(ctx, *, arg):
 @bot.command(aliases=["s"])
 async def stop(ctx):
     server_id = ctx.message.guild.id
-    vcid = vc_id.index(server_id)
-    if vcid != None:
-        if vc[vcid].is_playing():
-            vc[vcid].stop()
+    tcid = tc_id.index(server_id)
+    if tcid != None:
+        if vc[tcid].is_playing():
+            vc[tcid].stop()
             await ctx.send("Okay")
         else:
             await ctx.send("Cannot stop! No song is playing.")
@@ -221,10 +214,10 @@ async def stop(ctx):
 @bot.command()
 async def pause(ctx):
     server_id = ctx.message.guild.id
-    vcid = vc_id.index(server_id)
-    if vcid != None:
-        if vc[vcid].is_playing():
-            vc[vcid].pause()
+    tcid = tc_id.index(server_id)
+    if tcid != None:
+        if vc[tcid].is_playing():
+            vc[tcid].pause()
             await ctx.send("Okay")
         else:
             await ctx.send("Cannot pause! No song is playing.")
@@ -233,9 +226,9 @@ async def pause(ctx):
 @bot.command(aliases=["r"])
 async def resume(ctx):
     server_id = ctx.message.guild.id
-    vcid = vc_id.index(server_id)
-    if vcid != None:
-        vc[vcid].resume()
+    tcid = tc_id.index(server_id)
+    if tcid != None:
+        vc[tcid].resume()
         await ctx.send("Okay")
 
 
@@ -243,20 +236,21 @@ async def resume(ctx):
 async def refresh(ctx):
     global vc
     global vc_id
-    vcid = vc_id.index(ctx.message.guild.id)
+    global tc_id
+    tcid = tc_id.index(ctx.message.guild.id)
 
     try:
-        vc_id.remove(ctx.message.guild.id)
+        tc_id.remove(ctx.message.guild.id)
     except:
         print("Server wasn't in list of servers, so it can't be refreshed!")
 
     try:
-        vc[vcid].stop()
+        await vc[tcid].stop()
     except:
         print("Couldn't stop!")
 
     try:
-        vc[vcid].disconnect()
+        await vc[tcid].disconnect()
     except:
         print("Couldn't disconnect from a voice channel!")
 
@@ -266,7 +260,9 @@ async def refresh(ctx):
     #    print(e)
 
     try:
-        del vc[vcid]
+        del vc[tcid]
+        del vc_id[tcid]
+        del tc_id[tcid]
     except:
         print("Server wasn't in list of servers, so it can't be refreshed!")
 
@@ -281,7 +277,8 @@ async def about(ctx):
 @bot.command()
 async def lyrics(ctx, *, arg):
     lyrics = await harmonoid.getLyrics(trackName=arg, trackId=None)
-    if lyrics["lyricsFound"] == True:
+    print(lyrics)
+    if lyrics["lyrics"]:
         lyrics = lyrics["lyrics"]
         if len(lyrics) > 1999:
             await ctx.send(
@@ -302,9 +299,11 @@ async def lyricsSend(ctx, *, arg):
             f"[lyrics] Lyrics already downloaded for track ID: {trackId}.\n[server] Sending audio binary for track ID: {trackId}."
         )
         await ctx.send(file=discord.File(trackId + ".txt"))
-    elif lyrics["lyricsFound"] == True:
+    elif lyrics["lyrics"]:
         lyrics = lyrics["lyrics"]
-        logging.exception("\n\n--------\n\nLyrics exception: ")
+        filename = trackId + ".txt"
+        async with aiofiles.open(filename, "w", encoding="UTF-8") as file:
+            await file.write(lyrics)
         await ctx.send(file=discord.File(trackId + ".txt"))
 
 
@@ -312,21 +311,23 @@ async def lyricsSend(ctx, *, arg):
 async def disconnect(ctx):
     global vc
     global vc_id
-    vcid = vc_id.index(ctx.message.guild.id)
+    global tc_id
+    tcid = tc_id.index(ctx.message.guild.id)
 
     try:
-        await vc[vcid].stop()
+        await vc[tcid].stop()
     except:
         logging.exception("\n\n--------\n\[stop] Disconnect exception: ")
 
     try:
-        await vc[vcid].disconnect()
+        await vc[tcid].disconnect()
     except:
         logging.exception("\n\n--------\n\[disconnect] Disconnect exception: ")
         return
 
-    del vc_id[vcid]
-    del vc[vcid]
+    del tc_id[tcid]
+    del vc_id[tcid]
+    del vc[tcid]
 
     await ctx.send("Successfully disconnected from a voice channel :smiley: ")
 
@@ -334,24 +335,30 @@ async def disconnect(ctx):
 async def disconnectOnEmptyChannel():
     global vc
     global vc_id
+    global tc_id
 
-    await bot.wait_until_ready()  # ensures cache is loaded
-    for voice_id in vc_id:
-        vcid = vc_id.index(voice_id)
-        channel = bot.get_channel(id=voice_id)  # replace with target channel id
-        if len(channel.members) < 2:
-            try:
-                await vc[vcid].stop()
-            except:
-                print("Nothing is playing")
-            try:
-                await vc[vcid].disconnect()
-            except:
-                print("Failed to disconnect")
-        del vc_id[vcid]
-        del vc[vcid]
-    while not bot.is_closed():
+    while True:
+        try:
+            for voice_id in vc_id:
+                vcid = vc_id.index(voice_id)
+                await bot.wait_until_ready()
+                channel = bot.get_channel(id=voice_id)
+                if len(channel.members) < 2:
+                    try:
+                        await vc[vcid].stop()
+                    except:
+                        print("Nothing is playing")
+                    try:
+                        await vc[vcid].disconnect()
+                    except:
+                        print("Failed to disconnect")
+                    del tc_id[vcid]
+                    del vc_id[vcid]
+                    del vc[vcid]
+        except Exception as e:
+            print(e)
         await asyncio.sleep(600)  # Do it every 10 minutes
+
 
 if __name__ == "__main__":
     bot.run(TOKEN)
