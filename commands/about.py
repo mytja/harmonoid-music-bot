81257b1a2cd6456c0b3062652dc7dc10f3ff8c22
alexmercerind <saini123hitesh@gmail.com>
2021-02-04 22:06:01 +0530

    Added about command class

---
+++ commands/about.py
@@ -0,0 +1,10 @@
+from commands import *
+
+
+class About(Commands):
+    def __init__(self, bot):
+        super().__init__(bot)
+        
+    @commands.command()
+    async def about(self, ctx):
+        await self.embed.about(ctx)
