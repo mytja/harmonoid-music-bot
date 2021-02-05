29a4a1725d5e01bd4d050c126b4ce361d23129b2
alexmercerind <saini123hitesh@gmail.com>
2021-02-05 17:29:51 +0530

    Made single togglePlayback command for both play & pause

--- commands/controls.py
+++ commands/controls.py
@@ -6,24 +6,28 @@ class Controls(Commands):
     def __init__(self):
         super().__init__(Commands.bot)
 
-    @commands.command(aliases=['s'])
-    async def pause(self, ctx):
+    @commands.command(aliases=['pp'])
+    async def togglePlayback(self, ctx):
         if not (server := await Server.get(ctx)):
             return None
-        server.pause()
-        await self.embed.text(
-            ctx,
-            'Paused Music. ⏸',
-            '✅'
-        )
-
-    @commands.command(aliases=['r'])
-    async def resume(self, ctx):
-        if not (server := await Server.get(ctx)):
-            return None
-        server.resume()
-        await self.embed.text(
-            ctx,
-            'Resumed Music. ▶',
-            '✅'
-        )
+        if server.voiceConnection:
+            if server.voiceConnection.is_playing():
+                server.pause()
+                await self.embed.text(
+                    ctx,
+                    'Paused Music. ⏸',
+                    '✅'
+                )
+            else:
+                server.resume()
+                await self.embed.text(
+                    ctx,
+                    'Resumed Music. ▶',
+                    '✅'
+                )
+        else:
+            await self.embed.text(
+                ctx,
+                'Nothing is playing. ❌',
+                '❌'
+            )
